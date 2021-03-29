f7c2e2578456382a41bac0e3746eaf133ac559fa
penciso1 <63486016+penciso1@users.noreply.github.com>
2021-03-28 22:08:18 -0700

    created card components

--- main.js
+++ main.js
@@ -13,7 +13,60 @@ customElements.define(
   }
 );
 
+// variables
 const countrySection = document.getElementById("countries");
+
+// functions
+
+// get data from api
+const getData = async () => {
+  let response = await fetch("https://restcountries.eu/rest/v2/all");
+  let data = response.json();
+  return data;
+};
+
+const createCountryCard = (country) => {
+  // create card component
+  const card = document.createElement("country-card");
+  // create info Components
+  const flag = document.createElement("img");
+  const name = document.createElement("h3");
+  const population = document.createElement("span");
+  const region = document.createElement("span");
+  const capital = document.createElement("span");
+  // add info to components
+  flag.src = country.flag;
+  name.innerHTML = country.name;
+  population.innerHTML = country.population;
+  region.innerHTML = country.region;
+  capital.innerHTML = country.capital;
+  // add slot attribute to Components
+  flag.setAttribute("slot", "flag");
+  name.setAttribute("slot", "country");
+  population.setAttribute("slot", "population");
+  region.setAttribute("slot", "region");
+  capital.setAttribute("slot", "capital");
+  // add components to the country card component
+  card.appendChild(flag);
+  card.appendChild(name);
+  card.appendChild(population);
+  card.appendChild(region);
+  card.appendChild(capital);
+  // add country card to the country section
+  countrySection.appendChild(card);
+};
+
+// main logic
+const main = async () => {
+  const countryData = await getData();
+
+  for (i = 0; i < countryData.length; i++) {
+    createCountryCard(countryData[i]);
+  }
+};
+
+// calling functions
+main();
 
 /* to create a country-card component
   - create card component
