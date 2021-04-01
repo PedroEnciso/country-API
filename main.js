53fed328b9cdb6f99f09f79aefc6d5185c4e8bf4
penciso1 <63486016+penciso1@users.noreply.github.com>
2021-03-31 21:58:43 -0700

    finished the details page

--- main.js
+++ main.js
@@ -32,6 +32,21 @@ const countrySection = document.getElementById("countries");
 const inputArea = document.getElementById("input-area");
 const selectArea = document.getElementById("select-area");
 
+// detail page variables
+const backButton = document.getElementById("back-button");
+const detailScreen = document.getElementById("detail-screen");
+const detailFlag = document.getElementById("detail-flag");
+const detailCountry = document.getElementById("detail-country");
+const nativeName = document.getElementById("native-name");
+const population = document.getElementById("population");
+const region = document.getElementById("region");
+const subRegion = document.getElementById("sub-region");
+const capital = document.getElementById("capital");
+const domain = document.getElementById("domain");
+const language = document.getElementById("language");
+const currency = document.getElementById("currency");
+const buttonDiv = document.getElementById("buttons");
+
 // functions
 
 // get data from api
@@ -76,13 +91,79 @@ const createCountryCard = (country) => {
 
 const createDetailPage = (country) => {
   clearScreen();
-  console.log(country.name);
+  removeButtons();
+  detailFlag.src = country.flag;
+  detailCountry.innerHTML = country.name;
+  nativeName.innerHTML = country.nativeName;
+  population.innerHTML = country.population;
+  region.innerHTML = country.region;
+  subRegion.innerHTML = country.subregion;
+  capital.innerHTML = country.capital;
+  domain.innerHTML = country.topLevelDomain;
+  getCurrencies(country);
+  getLanguages(country);
+  createBorderButtons(country);
+};
+
+const createSpecificDetailPage = (name) => {
+  fetch(`https://restcountries.eu/rest/v2/alpha/${name}`)
+    .then((resp) => resp.json())
+    .then((data) => {
+      createDetailPage(data);
+    });
+};
+
+const getCurrencies = (country) => {
+  currencyList = "";
+  for (let i = 0; i < country.currencies.length; i++) {
+    currencyList += country.currencies[i].name;
+    if (i < country.currencies.length - 1) {
+      currencyList += ", ";
+    }
+  }
+  currency.innerHTML = currencyList;
+};
+
+const getLanguages = (country) => {
+  languageList = "";
+  for (let i = 0; i < country.languages.length; i++) {
+    languageList += country.languages[i].name;
+    if (i < country.languages.length - 1) {
+      languageList += ", ";
+    }
+  }
+  language.innerHTML = languageList;
+};
+
+const createBorderButtons = (country) => {
+  for (let i = 0; i < country.borders.length; i++) {
+    const button = document.createElement("BUTTON");
+    button.innerHTML = country.borders[i];
+    button.addEventListener("click", () => {
+      createSpecificDetailPage(button.innerHTML);
+    });
+    buttonDiv.appendChild(button);
+  }
 };
 
 const clearScreen = () => {
   inputArea.style.display = "none";
   selectArea.style.display = "none";
   countrySection.style.display = "none";
+  detailScreen.style.display = "block";
+};
+
+/////// CHANGE WHEN WORKING ON SELECT AREA ///////
+const backToHome = () => {
+  removeButtons();
+  inputArea.style.display = "block";
+  selectArea.style.display = "none";
+  countrySection.style.display = "block";
+  detailScreen.style.display = "none";
+};
+
+const removeButtons = () => {
+  buttonDiv.innerHTML = "";
 };
 
 // main logic
@@ -93,6 +174,9 @@ const main = async () => {
     createCountryCard(countryData[i]);
   }
 };
+
+//event listeners
+backButton.addEventListener("click", backToHome);
 
 // calling functions
 main();
